d2b4b0e31ee87cabc14c363bf582f461d53e605e
Samuel-Brito19 <sambriton7@gmail.com>
2024-02-01 15:36:47 -0300

    Modfying the label content

--- src/components/Car/array.ts
+++ src/components/Car/array.ts
@@ -1,6 +1,6 @@
 export const CarsArray = [
-{label: 'Dodge Ram', image: 'https://www.ram.com.br/content/dam/ram/products/570/profile.png.thumb.1280.1280.png'},
-{label: 'Honda Civic', image: 'https://opopular.com.br/image/policy%3A1.2133439%3A1678101760%2Fimage.jpg%3F%26f%3D3x2'},
-{label: 'Nivus', image: 'https://blog.carrera.com.br/wp-content/uploads/2023/09/image-27-1024x614.png'},
-{label: 'Honda HRV', image: 'https://img.sm360.ca/images/article/groupe-duplessis-honda/83098//2020-honda-hr-v-sport-004-source1611605978590.jpg'}
+{label: 'Dodge Ram 2018 $400/day', image: 'https://www.ram.com.br/content/dam/ram/products/570/profile.png.thumb.1280.1280.png'},
+{label: 'Honda Civic 2021 $200/day', image: 'https://opopular.com.br/image/policy%3A1.2133439%3A1678101760%2Fimage.jpg%3F%26f%3D3x2'},
+{label: 'Nivus 2022 $150/day', image: 'https://blog.carrera.com.br/wp-content/uploads/2023/09/image-27-1024x614.png'},
+{label: 'Honda HRV 2020 $180/day', image: 'https://img.sm360.ca/images/article/groupe-duplessis-honda/83098//2020-honda-hr-v-sport-004-source1611605978590.jpg'}
 ]
--- src/components/Car/styles.ts
+++ src/components/Car/styles.ts
@@ -1,9 +1,15 @@
 import styled from "styled-components";
 
-export const WrapCars = styled.div`
+export const WrapCars = styled.button`
 display: flex;
 flex-direction: row;
 gap: 20px;
+width: 100%;
+background-color: #FFFFFF;
+border: 1px solid black;
+border-radius: 10px;
+align-items: center;
+justify-content: center;
 `
 
 export const LabelCars = styled.label`
@@ -13,7 +19,11 @@ height: 100px;
 `
 
 export const ImgCar = styled.img`
-width: 100px;
+width: 150px;
 align-items: flex-end;
-height: 100px;
+height: 75px;
+`
+
+export const NormalLabel = styled.label`
+padding-top: 60px;
 `
