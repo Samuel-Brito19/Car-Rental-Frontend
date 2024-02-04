75799b9cb4a7976dc822ac2f0a6a74d83ca147dc
Samuel-Brito19 <sambriton7@gmail.com>
2024-02-04 19:59:22 -0300

    Creating our folders

--- src/pages/CarDetails/index.tsx
+++ src/pages/CarDetails/index.tsx
@@ -1,9 +1,13 @@
-import { Container, ImageContainer } from "./styles"
+import { CarDescription, CarName, Container, DetailsContainer, ImageContainer } from "./styles"
 
 const CarDetails = () => {
     return (
         <Container>
             <ImageContainer/>
+            <DetailsContainer>
+                <CarName>Fox</CarName> 
+                <CarDescription>como colocar um read more?</CarDescription>
+            </DetailsContainer>
         </Container>
     )
 }
--- src/pages/CarDetails/styles.ts
+++ src/pages/CarDetails/styles.ts
@@ -4,6 +4,8 @@ export const Container = styled.div`
 display: flex;
 align-items: center;
 justify-content: center;
+flex-direction: column;
+gap: 20px;
 
 `
 
@@ -11,12 +13,28 @@ export const ImageContainer = styled.div`
 /* display: flex;
 align-items: center;
 justify-content: center; */
-width: 300px;
-height: 200px;
-background-color: red;
+width: 400px;
+height: 300px;
+background-color: white;
 margin-top: 100px;
+border: 2px solid none;
+border-radius: 20px;
 `
 
 export const DetailsContainer = styled.div`
-
+width: 400px;
+height: 400px;
+background-color: blue;
+border: 2px solid none;
+border-radius: 20px;
 `
+export const CarName = styled.h2`
+font-size: 30px;
+padding: 20px;
+`
+
+export const CarDescription = styled.label`
+font-size: 20px;
+padding: 20px;
+`
+
