1cf3333f622f6713907c0a47c5fa782cd4467b18
Samuel-Brito19 <sambriton7@gmail.com>
2024-02-09 20:06:36 -0300

    Logic into Login

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import ComponentCar from './pages/AvailableCars'
 import { CarsArray } from './pages/AvailableCars/array'
 import CarDetails from './pages/CarDetails'
+import Login from './pages/Login'
 import './styles/global.css'
 
 function App() {
   return (
-    <ComponentCar CarsImages={CarsArray}/>
+    <Login/>
   )
 }
 
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,9 +1,20 @@
+import { Link, useNavigate } from "react-router-dom"
 import { Button } from "../../components/Button/styles"
 import { ContainerForm } from "../../components/Form/style"
 import { InputStyled } from "../../components/Input/style"
 import { Container, Header, Title } from "../Register/styles"
+import { useState } from "react"
+import { Span } from "./styles"
 
 const Login = () => {
+    //const navigate = useNavigate()
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
+    }
     
     return (
         <>
@@ -16,6 +27,9 @@ const Login = () => {
             <InputStyled placeholder="Insira seu email"/>
             <InputStyled placeholder="Insira sua senha"/>
             <Button>Entrar</Button>
+            <Span>
+          Don't have an account yet? <Link to="/register">Register</Link>
+            </Span>
         </ContainerForm>
         </>
     )
