3018d34c87b4e6476a8cce1b834ab22ed847e886
Samuel-Brito19 <sambriton7@gmail.com>
2024-02-18 15:36:24 -0300

    Installing mui/material

--- src/pages/RentInputs/index.tsx
+++ src/pages/RentInputs/index.tsx
@@ -4,14 +4,22 @@ import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import { WrapInputRow } from '../AvailableCars/styles';
+import { TimePicker } from '@mui/x-date-pickers';
 
 export default function BasicDatePicker() {
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
+       <Autocomplete
+      disablePortal
+      id="combo-box-demo"
+      options={top100Films}
+      sx={{ width: 300 }}
+      renderInput={(params) => <TextField {...params} label="Movie" />}
+    />
       <DemoContainer components={['DatePicker']}>
         <WrapInputRow>
         <DatePicker label="Basic date picker" />
-        <DatePicker label="Basic date picker" />
+        <TimePicker label="Basic time picker" />
         </WrapInputRow>
         
       </DemoContainer>
