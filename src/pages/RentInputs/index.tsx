3aef66d94b7171a14b7cbddbba7478c9b3b6d2e5
Samuel-Brito19 <sambriton7@gmail.com>
2024-02-18 10:35:32 -0300

    Date picker component

--- src/pages/RentInputs/index.tsx
+++ src/pages/RentInputs/index.tsx
@@ -3,12 +3,17 @@ import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { WrapInputRow } from '../AvailableCars/styles';
 
 export default function BasicDatePicker() {
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
       <DemoContainer components={['DatePicker']}>
+        <WrapInputRow>
         <DatePicker label="Basic date picker" />
+        <DatePicker label="Basic date picker" />
+        </WrapInputRow>
+        
       </DemoContainer>
     </LocalizationProvider>
   );
